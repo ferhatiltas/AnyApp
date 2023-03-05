f8a187614c1440b733ce173fca49aed3363ae413
Ferhat Iltas <iltasferhat@gmail.com>
2023-03-06 00:45:44 +0300

    Initial Commit

---
+++ AnyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnyApp
+//
+//  Created by ferhatiltas on 6.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
